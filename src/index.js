d4b5e1c97bb57eda3802767a3fa69275080a3e37
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 17:52:00 +1000

    bug fix in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(library, message, number) {
+function Hello({ library, message, number }) {
   return (
     <div>
       <h1>Welcome to {library}!</h1>
@@ -13,6 +13,10 @@ function Hello(library, message, number) {
 }
 
 ReactDOM.render(
-  <Hello library="React" message="Have fun" number={3} />,
+  <Hello 
+    library="React" 
+    message="Have fun" 
+    number={3} 
+  />,
   document.getElementById('root')
 );
