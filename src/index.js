598dd85fd668e129e44f4b5bd824618a58da8ac7
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 18:40:29 +1000

    state variable for manager added

--- src/index.js
+++ src/index.js
@@ -3,20 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
+  const [manager, setManager] = useState("Alex")
   const [status, setStatus] = useState("Open")
   return (
-    <div>
-      <h1>Status: {status}</h1>
-      <button onClick={() => setStatus("Open")}>
-        Open
-      </button>
-      <button onClick={() => setStatus("Back in 5")}>
-        Break
-      </button>
-      <button onClick={() => setStatus("Closed")}>
-        Closed
-      </button>
-    </div> 
+    <>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}>
+          New Manager
+        </button>
+      </div>
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>
+          Open
+        </button>
+        <button onClick={() => setStatus("Back in 5")}>
+          Break
+        </button>
+        <button onClick={() => setStatus("Closed")}>
+          Closed
+        </button>
+      </div> 
+    </>  
   )
 }
 
