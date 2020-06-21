42c59191c12f3e91980b7ad48f006405847227f5
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 18:42:53 +1000

    year and setYear added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
+  const [year, setYear] = useState(2050)
   const [manager, setManager] = useState("Alex")
   const [status, setStatus] = useState("Open")
   return (
     <>
+      <div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>
+          New Year!
+        </button>
+      </div>
       <div>
         <h1>Manager on Duty: {manager}</h1>
         <button onClick={() => setManager("Rachel")}>
