7206cfd693bc79eec543cb3394d8ba1fb8370917
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 17:44:22 +1000

    props added to index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
+function Hello(props) {
+  console.log(props)
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to {props.library}!</h1>
       <p>Let's build something cool.</p>
     </div> 
   )
 }
 
 ReactDOM.render(
-  <Hello />,
+  <Hello library="React" />,
   document.getElementById('root')
 );
