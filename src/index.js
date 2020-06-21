06b9af70554209237a8926ad8e90ccedd848fc0f
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 18:15:54 +1000

    implementing conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const list = [1, 2, 3, 4, 5]
-
-function App({items}) {
+function Lake() {
   return (
-   <ul>
-     {items.map(item => (
-      <li key={item.toString()}>{item}</li>
-     ))}
-   </ul>
+  <div>
+    <h1> Visit Jenny Lake!</h1>
+  </div>
+  )
+} 
+
+function SkiResort() {
+  return (
+    <div>
+      <h1>Visit Jackson Hole Mountain Resort!</h1>
+    </div>
   )
 }
 
+function App(props) {
+  if(props.season === "summer") {
+    return <Lake />
+  }
+  return <SkiResort />
+}
+
 ReactDOM.render(
-  <App items={list}/>,
+  <App season="summer"/>,
   document.getElementById('root')
 );
