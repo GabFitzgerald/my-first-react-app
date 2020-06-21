31c18273e690dcbed4f792cf1dbf62e042a9c7af
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 20:06:17 +1000

    checkbox added using useState

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function GitHubUser({login}) {
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error)
-  }, [])
-
-  if(data) {
-    return (
-      <div>
-        <h1>{data.login}</h1>
-        <img src={data.avatar_url} width={100} />
-      </div>
-    )
-  }
-  return null
-}
-
-function App() {
-  return <GitHubUser login="eveporcello" />
+function Checkbox() {
+  const [checked, setChecked] = useState(false)
+  return (
+    <>
+      <input type="checkbox" value ={checked} />
+      {checked ? "checked" : "not checked"}
+    </>
+  )
 }
 
 ReactDOM.render(
-  <App />,
+  <Checkbox />,
   document.getElementById('root')
 );
