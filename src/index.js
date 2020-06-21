8876d9243c22e9d0ab157bbb03045631bda6ca0e
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-21 19:54:42 +1000

    Updating the useEffect dependency array

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
+  const [val, setVal] = useState("")
+  const [val2, setVal2] = useState("")
+  
+  useEffect(() => {
+    console.log(`field 1: ${val}`)
+  }, [val])
+  
+  useEffect(() => {
+    console.log(`field 2: ${val2}`)
+  }, [val2])
+
   return (
     <>
       <label>
         Favorite Phrase:
-        <input />
+        <input 
+          value={val} 
+          onChange={e => setVal(e.target.value)}
+        />
       </label>
+      <br />
       <label>
         Second Favorite Phrase:
-        <input />
+        <input 
+          value={val2}
+          onChange={e => setVal2(e.target.value)}
+        />
       </label>
     </>
   )
